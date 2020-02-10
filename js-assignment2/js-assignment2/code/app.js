9337b62c23ecdf39f49d55f838419826390a5b72
Gander-Alexander <Gander-Alexander@users.noreply.github.com>
2020-02-10 13:32:18 -0700

    Create app.js

---
+++ js-assignment2/js-assignment2/code/app.js
@@ -0,0 +1,10 @@
+const burgerMenu = document.querySelector('.hamburgerMenu')
+
+const navLinks = document.querySelector('.links')
+
+burgerMenu.addEventListener('click', function(){
+   navLinks.classList.toggle('active')
+  burgerMenu.classList.toggle('toggle')
+})
+console.log(navLinks)
+
